dda9cbf9b004492281fa7bce15be5d6883b9bd4a
mobs199 <beshtawi@ewanto.de>
2024-12-20 17:07:43 +0100

    var to let main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 window.onload = function() {
-  var dropdown = document.getElementById('quantity-range');
+  let dropdown = document.getElementById('quantity-range');
   
 
-  for (var i = 1; i <= 200; i++) {
-      var start = (i - 1) * 100 + 1;
-      var end = i * 100; 
-      var option = document.createElement('option');
+  for (let i = 1; i <= 200; i++) {
+      let start = (i - 1) * 100 + 1;
+      let end = i * 100; 
+      let option = document.createElement('option');
       option.value = start + '-' + end;
       option.textContent = start + ' bis ' + end;
       dropdown.appendChild(option);
@@ -13,21 +13,21 @@ window.onload = function() {
 };
 
 document.getElementById('quantity-range').addEventListener('change', function() {
-  var secondDropdownContainer = document.getElementById('second-dropdown-container');
-  var exactQuantityDropdown = document.getElementById('exact-quantity');
+  let secondDropdownContainer = document.getElementById('second-dropdown-container');
+  let exactQuantityDropdown = document.getElementById('exact-quantity');
   
   exactQuantityDropdown.innerHTML = '<option value="">Bitte wählen</option>';
   
-  var selectedRange = this.value;
+  let selectedRange = this.value;
   
   if (selectedRange) {
       secondDropdownContainer.style.display = 'block'; 
       
-      var start = parseInt(selectedRange.split('-')[0]);
-      var end = parseInt(selectedRange.split('-')[1]);
+      let start = parseInt(selectedRange.split('-')[0]);
+      let end = parseInt(selectedRange.split('-')[1]);
 
-      for (var i = start; i <= end; i++) {
-          var option = document.createElement('option');
+      for (let i = start; i <= end; i++) {
+          let option = document.createElement('option');
           option.value = i;
           option.textContent = i;
           exactQuantityDropdown.appendChild(option);
